cdb6399b619acda7ddef47b44f7ba036985d6654
zakzakst <e1433104@gmail.com>
2024-04-06 14:15:24 +0900

    spacing

--- src/constants/mixins/spacing.ts
+++ src/constants/mixins/spacing.ts
@@ -1,5 +1,5 @@
-export const spacingProperties = ["margin", "padding"] as const;
-export const spacingDirections = [
+const spacingProperties = ["margin", "padding"] as const;
+const spacingDirections = [
   "top",
   "right",
   "bottom",
@@ -8,6 +8,19 @@ export const spacingDirections = [
   "horizontal",
   "all",
 ] as const;
+const SPACING = {
+  0: 0,
+  4: 4,
+  8: 8,
+  16: 16,
+  24: 24,
+  40: 40,
+  64: 64,
+  104: 104,
+  168: 168,
+  272: 272,
+  440: 440,
+} as const;
 type SpacingProperty = (typeof spacingProperties)[number];
 type SpacingDirection = (typeof spacingDirections)[number];
 
@@ -17,3 +30,53 @@ const spacingProperty = (
 ) => {
   return `${property}-${direction}`;
 };
+
+type SpacingObj = Partial<
+  Record<"top" | "right" | "bottom" | "left", keyof typeof SPACING>
+>;
+
+const createSpacingCss = (
+  property: SpacingProperty,
+  modifiers: [SpacingDirection, keyof typeof SPACING][]
+) => {
+  const result = modifiers.reduce(
+    (acc, [direction, size]) => {
+      if (direction === "all") {
+        const spacingObj: SpacingObj = {
+          top: size,
+          right: size,
+          bottom: size,
+          left: size,
+        };
+        acc[direction] = spacingObj;
+      } else if (direction === "vertical") {
+        const spacingObj: SpacingObj = {
+          top: size,
+          bottom: size,
+        };
+        acc[direction] = spacingObj;
+      } else if (direction === "horizontal") {
+        const spacingObj: SpacingObj = {
+          right: size,
+          left: size,
+        };
+        acc[direction] = spacingObj;
+      } else {
+        const spacingObj: SpacingObj = {
+          [direction]: size,
+        };
+        acc[direction] = spacingObj;
+      }
+      return acc;
+    },
+    {} as Record<SpacingDirection, SpacingObj>
+  );
+  return result;
+};
+
+console.log(
+  createSpacingCss("margin", [
+    ["all", 0],
+    ["top", 4],
+  ])
+);
