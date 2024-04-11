ef2c99a23896463d4bad979bb754c7353f0447f8
zakzakst <e1433104@gmail.com>
2024-04-12 07:00:40 +0900

    Link テストコード

---
+++ __tests__/Link.test.tsx
@@ -0,0 +1,35 @@
+// https://github.com/adobe/react-spectrum/blob/main/packages/react-aria-components/test/Link.test.js
+describe("Link", () => {
+  // デフォルトのクラスが設定される
+  it("should render a link with default class", () => {});
+
+  // カスタムクラスが設定される
+  it("should render a link with custom class", () => {});
+
+  // DOMに属性が設定される
+  it("should support DOM props", () => {});
+
+  // 描画スクリプトが反映される
+  it("should support render props", () => {});
+
+  // slot属性を利用できる
+  it("should support slot", () => {});
+
+  // aタグでリンクが描画される
+  it("should render a link with <a> element", () => {});
+
+  // ホバー時の処理が実行される
+  it("should support hover", () => {});
+
+  // フォーカス時に線が表示される
+  it("should support focus ring", () => {});
+
+  // 押した時に状態が変化する
+  it("should support press state", () => {});
+
+  // 無効状態が設定される
+  it("should support disabled state", () => {});
+
+  // RouterProviderが挙動する
+  it("should work with RouterProvider", () => {});
+});
