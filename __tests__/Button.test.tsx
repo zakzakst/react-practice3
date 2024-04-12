501d3d4b9ce8b3e68feb3c1fec9657301bc2bc38
zakzakst <e1433104@gmail.com>
2024-04-13 04:44:41 +0900

    テストコード

--- __tests__/Button.test.tsx
+++ __tests__/Button.test.tsx
@@ -1,23 +1,20 @@
-// https://github.com/adobe/react-spectrum/blob/main/packages/react-aria-components/test/Link.test.js
+// https://github.com/adobe/react-spectrum/blob/main/packages/react-aria-components/test/Button.test.js
 
-describe("Link", () => {
+describe("Button", () => {
   // デフォルトのクラスが設定される
-  it("should render a link with default class", () => {});
+  it("should render a button with default class", () => {});
 
   // カスタムクラスが設定される
-  it("should render a link with custom class", () => {});
+  it("should render a button with custom class", () => {});
 
   // DOMに属性が設定される
   it("should support DOM props", () => {});
 
-  // 描画スクリプトが反映される
-  it("should support render props", () => {});
+  // フォーム属性に対応している
+  it("should support form props", () => {});
 
   // slot属性を利用できる
   it("should support slot", () => {});
-
-  // aタグでリンクが描画される
-  it("should render a link with <a> element", () => {});
 
   // ホバー時の処理が実行される
   it("should support hover", () => {});
@@ -31,6 +28,6 @@ describe("Link", () => {
   // 無効状態が設定される
   it("should support disabled state", () => {});
 
-  // RouterProviderが挙動する
-  it("should work with RouterProvider", () => {});
+  // 描画スクリプトが反映される
+  it("should support render props", () => {});
 });
