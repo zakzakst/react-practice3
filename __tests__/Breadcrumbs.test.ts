d6d1d7b590c6b62b319a1d2fd56760ab6d32ce73
zakzakst <e1433104@gmail.com>
2024-04-13 11:55:18 +0900

    テストコード

---
+++ __tests__/Breadcrumbs.test.ts
@@ -0,0 +1,21 @@
+// https://github.com/adobe/react-spectrum/blob/main/packages/react-aria-components/test/Breadcrumbs.test.js
+
+describe("Breadcrumbs", () => {
+  // デフォルトのクラスが設定される
+  it("should render with default class", () => {});
+
+  // カスタムクラスが設定される
+  it("should render with custom class", () => {});
+
+  // DOMに属性が設定される
+  it("should support DOM props", () => {});
+
+  // slot属性を利用できる
+  it("should support slot", () => {});
+
+  // 配列を参照できる
+  it("should support dynamic collections", () => {});
+
+  // ref属性を利用できる
+  it("should support refs", () => {});
+});
---
+++ __tests__/Checkbox.test.tsx
@@ -0,0 +1,57 @@
+// https://github.com/adobe/react-spectrum/blob/main/packages/react-aria-components/test/Checkbox.test.js
+
+describe("Checkbox", () => {
+  // デフォルトのクラスが設定される
+  it("should render a checkbox with default class", () => {});
+
+  // カスタムクラスが設定される
+  it("should render a checkbox with custom class", () => {});
+
+  // DOMに属性が設定される
+  it("should support DOM props", () => {});
+
+  // slot属性を利用できる
+  it("should support slot", () => {});
+
+  // ホバー時の処理が実行される
+  it("should support hover", () => {});
+
+  // フォーカス時に線が表示される
+  it("should support focus ring", () => {});
+
+  // フォーカスイベントを利用できる
+  it("should support focus events", () => {});
+
+  // 押した時に状態が変化する
+  it("should support press state", () => {});
+
+  // 無効状態が設定される
+  it("should support disabled state", () => {});
+
+  // 選択状態が設定される
+  it("should support selected state", () => {});
+
+  // 未確定状態が設定される
+  it("should support indeterminate state", () => {});
+
+  // 読み取り専用状態が設定される
+  it("should support read only state", () => {});
+
+  // バリデーション失敗状態が設定される
+  it("should support invalid state", () => {});
+
+  // 入力必須状態が設定される
+  it("should support required state", () => {});
+
+  // 描画スクリプトが反映される
+  it("should support render props", () => {});
+
+  // ref属性を利用できる
+  it("should support refs", () => {});
+
+  // inputのref属性を利用できる
+  it("should support input ref", () => {});
+
+  //
+  it("should support and merge input ref on context", () => {});
+});
