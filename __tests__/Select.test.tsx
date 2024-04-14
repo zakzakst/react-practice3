7c68bbacfabd3b52c609e3026156e2862262cd30
zakzakst <e1433104@gmail.com>
2024-04-14 17:00:53 +0900

    テストコード

---
+++ __tests__/Select.test.tsx
@@ -0,0 +1,30 @@
+// https://github.com/adobe/react-spectrum/blob/main/packages/react-aria-components/test/Select.test.js
+
+describe("Select", () => {
+  //
+  it("provides slots", () => {});
+
+  // slot属性を利用できる
+  it("should support slot", () => {});
+
+  // itemsの描画スクリプトが反映される
+  it("supports items with render props", () => {});
+
+  //
+  it("supports custom select value", () => {});
+
+  // placeholderを利用できる
+  it("supports placeholder", () => {});
+
+  // 描画スクリプトが反映される
+  it("should support render props", () => {});
+
+  //
+  it("should send disabled prop to the hidden field", () => {});
+
+  //
+  it("should render data- attributes only on the outer element", () => {});
+
+  // バリデーションエラーを利用できる
+  it("supports validation errors", () => {});
+});
