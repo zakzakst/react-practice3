797f60a2acdc9cfdac88a8fab42bf66fbdfd278f
zakzakst <e1433104@gmail.com>
2024-04-20 23:13:15 +0900

    テストコード

---
+++ __tests__/DropZone.test.tsx
@@ -0,0 +1,42 @@
+// https://github.com/adobe/react-spectrum/blob/main/packages/react-aria-components/test/DropZone.test.js
+
+describe("DropZone", () => {
+  // ドロップ領域が描画される
+  it("should render a dropzone", () => {});
+
+  // カスタムクラスが設定される
+  it("should render a dropzone with custom class", () => {});
+
+  // DOMに属性が設定される
+  it("should support DOM props", () => {});
+
+  // slot属性を利用できる
+  it("should support slot", () => {});
+
+  // ホバー時の処理が実行される
+  it("should support hover", () => {});
+
+  // フォーカス時に線が表示される
+  it("should support focus ring", () => {});
+
+  // デフォルトのaria-labelledbyが設定される
+  it("should apply correct default aria-labelledby", () => {});
+
+  // カスタムaria-labelを利用できる
+  it("should allow custom aria-label", () => {});
+
+  // 描画スクリプトが反映される
+  it("should support render props", () => {});
+
+  //
+  it("should render FileTrigger as a child", () => {});
+
+  //
+  it("should attach a ref to the dropzone if provided as a prop", () => {});
+
+  // ドラッグドロップできる
+  it("drag and drop", () => {});
+
+  // クリップボードを利用できる
+  it("useClipboard", () => {});
+});
---
+++ __tests__/FileTrigger.test.tsx
@@ -0,0 +1,21 @@
+// https://github.com/adobe/react-spectrum/blob/main/packages/react-aria-components/test/FileTrigger.test.js
+
+describe("FileTrigger", () => {
+  //
+  it("", () => {});
+
+  //
+  it("", () => {});
+
+  //
+  it("", () => {});
+
+  //
+  it("", () => {});
+
+  //
+  it("", () => {});
+
+  //
+  it("", () => {});
+});
