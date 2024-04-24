1849b6b5a5bc00de44ce62b45e64558bf400703f
zakzakst <e1433104@gmail.com>
2024-04-24 22:10:02 +0900

    テストコード

---
+++ __tests__/Form.test.tsx
@@ -0,0 +1,6 @@
+// https://github.com/adobe/react-spectrum/blob/main/packages/react-aria-components/test/Form.test.js
+
+describe("Form", () => {
+  //
+  it("supports server validation errors", () => {});
+});
---
+++ __tests__/GridList.ssr.test.tsx
@@ -0,0 +1,6 @@
+// https://github.com/adobe/react-spectrum/blob/main/packages/react-aria-components/test/GridList.ssr.test.js
+
+describe("GridList SSR", () => {
+  // エラーが起きずに描画される
+  it("should render without errors", () => {});
+});
