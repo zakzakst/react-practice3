95f0e50a74445b069cbffbdc3ed775547f597ace
zakzakst <e1433104@gmail.com>
2024-05-10 21:23:04 +0900

    cryptoテスト

--- src/app/(ja)/test-components/page.tsx
+++ src/app/(ja)/test-components/page.tsx
@@ -1,7 +1,10 @@
+"use client";
 import AtButton from "@/components/at/button";
 import MoCounterButton from "@/components/mo/counterButton";
 
 export default function TestComponents() {
+  console.log(window.crypto.randomUUID());
+  console.log(window.crypto.randomUUID());
   return (
     <div>
       <p>test components</p>
